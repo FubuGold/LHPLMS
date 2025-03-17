fb26c48bf293b71fcf833fb31393aef2b480d9d3
BlackJackMC <duytu85343@gmail.com>
2025-03-17 21:36:01 +0700

    feat(gateway): create endpoints

--- apps/api-gateway/src/app/routes/subject.controller.js
+++ apps/api-gateway/src/app/routes/subject.controller.js
@@ -1,8 +1,37 @@
-import { Controller, Dependencies } from '@nestjs/common';
+import {
+    Bind,
+    Controller,
+    Delete,
+    Dependencies,
+    Get,
+    Param,
+    Patch,
+    Post,
+} from '@nestjs/common';
 import { SubjectService } from '../../domain/services/subject.service';
 
-@Controller('subject')
+@Controller('subjects')
 @Dependencies(SubjectService)
 export class SubjectController {
+    constructor(SubjectService) {
+        this.SubjectService = SubjectService;
+    }
 
+    @Get()
+    getAll() {}
+
+    @Get(':id')
+    @Bind(Param('id'))
+    getOne() {}
+
+    @Post()
+    createSubject() {}
+
+    @Patch(':id')
+    @Bind(Param('id'))
+    updateSubject(id) {}
+
+    @Delete(':id')
+    @Bind(Param('id'))
+    deleteSubject(id) {}
 }
