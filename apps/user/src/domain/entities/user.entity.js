58bcb8960ae9d707a023b98c2c4fa578ef64c7b2
Lam Tran Hoang <tranlam1750@gmail.com>
2025-03-18 23:03:21 +0700

    chore(user): update usertask, update user entity

--- apps/user/src/domain/entities/user.entity.js
+++ apps/user/src/domain/entities/user.entity.js
@@ -5,6 +5,11 @@ export class User {
         this.username = obj.username;
         this.dob = obj.dob;
         this.avatar = obj.avatar;
+        this.gender = obj.gender;
+        this.phoneNumber = obj.phoneNumber;
+        this.email = obj.email;
+        this.roleId = obj.roleId;
+        this.role = obj.role;
         Object.freeze(this);
     }
 }
