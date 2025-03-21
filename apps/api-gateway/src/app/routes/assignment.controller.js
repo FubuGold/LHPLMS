6ade47d2f12622d509ce3be948d3dce61f269e11
FubuGold <trangiahuy211207@gmail.com>
2025-03-22 00:09:15 +0700

    chore(assignment): fixed dependency, test controller

--- apps/api-gateway/src/app/routes/assignment.controller.js
+++ apps/api-gateway/src/app/routes/assignment.controller.js
@@ -15,6 +15,12 @@ export class AssignmentController {
         return await this.assignmentService.create({ ...body, classId: classId });
     }
 
+    // @Get('test')
+    // @Bind(Param('classId'))
+    // test(classId) {
+    //     return `Test: ${classId}`
+    // }
+
     @Get(':assignmentId')
     async getOne(id) {
         return await this.assignmentService.getOne(id);
