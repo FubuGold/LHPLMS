c07605b47b8f8b5d4db2a0a89458eac23c401d75
BlackJackMC <duytu85343@gmail.com>
2025-03-24 23:51:51 +0700

    feat(db): update db

--- prisma/migrations/20250224170434_user_token_constraint/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userId]` on the table `UserToken` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "UserToken_userId_key" ON "UserToken"("userId");
